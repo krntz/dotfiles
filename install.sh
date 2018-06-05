c5630ef90dfe9b51a19f35dc09b89e25cdd2f6de
Amandus Krantz <krantz.amandus@gmail.com>
2018-06-05 22:46:16 +0200

    install script now downloads vim colorscheme, it is no longer identical to update

--- install.sh
+++ install.sh
@@ -14,3 +14,9 @@ rm ~/.bashrc
 # symlink dotfiles
 ln -s $dotdir/vimrc ~/.vimrc
 ln -s $dotdir/bashrc ~/.vimrc
+
+# make sure vim has the correct colorscheme
+mkdir -p ~/.vim/color
+
+wget https://github.com/romainl/flattened/raw/master/colors/flattened_dark.vim
+mv flattened_dark.vim ~/.vim/color
