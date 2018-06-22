a220921b3926cf46940ddbf6fc476fe0051015f9
Amandus Krantz <krantz.amandus@gmail.com>
2018-06-22 13:46:40 +0200

    added gitconfig

--- install.sh
+++ install.sh
@@ -11,6 +11,7 @@ dotdir=$(pwd)
 # delete existing files
 rm ~/.vimrc
 rm ~/.bashrc
+rm ~/.gitconfig
 rm ~/.ssh/config
 rm -rf ~/.vim
 rm -rf ~/.termux
@@ -18,6 +19,7 @@ rm -rf ~/.termux
 # symlink dotfiles
 ln -s $dotdir/vimrc ~/.vimrc
 ln -s $dotdir/bashrc ~/.bashrc
+ln -s $dotdir/gitconfig ~/.gitconfig
 ln -s $dotdir/ssh/config ~/.ssh/config
 ln -s $dotdir/vim ~/.vim
 ln -s $dotdir/termux ~/.termux
--- update.sh
+++ update.sh
@@ -8,6 +8,7 @@ dotdir=$(pwd)
 # delete old .files
 rm ~/.vimrc
 rm ~/.bashrc
+rm ~/.gitconfig
 rm ~/.ssh/config
 rm -rf ~/.vim
 rm -rf ~/.termux
@@ -15,6 +16,7 @@ rm -rf ~/.termux
 # symlink new .files
 ln -s $dotdir/vimrc ~/.vimrc
 ln -s $dotdir/bashrc ~/.bashrc
+ln -s $dotdir/gitconfig ~/.gitconfig
 ln -s $dotdir/ssh/config ~/.ssh/config
 ln -s $dotdir/vim ~/.vim
 ln -s $dotdir/termux ~/.termux
