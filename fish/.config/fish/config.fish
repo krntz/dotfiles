026d75d015df9a93579b85cf713c161dbcc55221
Amandus Krantz <krantz.amandus@protonmail.com>
2023-06-19 15:57:13 +0200

    removed pyenv stuff

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,16 +1,4 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
-    if type -q pyenv
-        set -gx PYENV_VIRTUALENV_DISABLE_PROMPT 1
-        set -Ux PYENV_ROOT $HOME/.pyenv
-        set -U fish_user_paths $PYENV_ROOT/bin $fish_user_paths
-    end
-
     set -gx LANG en_US.UTF-8
-end
-
-if type -q pyenv
-    alias brew="env PATH=(string replace (pyenv root)/shims '' \"\$PATH\") brew"
-    status is-login; and pyenv init --path | source
-    status is-interactive; and pyenv init - | source
 end
--- fish/.config/fish/functions/fish_right_prompt.fish
+++
@@ -1,8 +0,0 @@
-function fish_right_prompt
-    if type -q pyenv
-        set pyenv_version (pyenv version-name)
-        if [ $pyenv_version != 'system:2.7.18' ]
-            echo "$pyenv_version "
-        end
-    end
-end
