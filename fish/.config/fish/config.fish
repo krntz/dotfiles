6e4c30f02752849876da618634c409111cbea153
Amandus Krantz <krantz.amandus@protonmail.com>
2024-06-15 11:13:23 +0200

    added fish config

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,16 @@
+if status is-interactive
+    # Commands to  run in interactive sessions can go here
+end
+
+set -g fish_greeting ""
+
+abbr -a --position command v "nvim"
+abbr -a --position command py "python"
+abbr -a --position command gc "git commit"
+abbr -a --position command gco "git checkout"
+abbr -a --position command gd "git diff"
+abbr -a --position command ga "git add"
+abbr -a --position command gp "git push"
+abbr -a --position command gpl "git pull"
+abbr -a --position command gcl "git clone"
+abbr -a --position command gs "git status"
