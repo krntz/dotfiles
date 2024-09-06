c5527dbda4c6bbe332bb9f0a086ae54a112ebcc0
Amandus Krantz <me@amanduskrantz.com>
2024-09-06 09:18:45 +0200

    fixed tabbar

--- vim/.config/nvim/lua/plugins/ui.lua
+++ vim/.config/nvim/lua/plugins/ui.lua
@@ -36,13 +36,17 @@ return {
 		"romgrk/barbar.nvim",
 		dependencies = {
 			"nvim-tree/nvim-web-devicons",
+			"lewis6991/gitsigns.nvim",
 		},
 		init = function()
 			vim.g.barbar_auto_setup = false
 		end,
-		opts = {
-			auto_hide = 1,
-		},
+        opts = {
+            auto_hide = true,
+            sidebar_filetypes = {
+                NvimTree = true,
+            }
+        }
 	},
 	{
 		"nvim-treesitter/nvim-treesitter",
